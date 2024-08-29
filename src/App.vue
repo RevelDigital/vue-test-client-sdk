20f64848733aa0999909ef414eff167bdd77fb9b
Avery knight <averjk@gmail.com>
2024-08-29 14:19:06 -0500

    added change detection

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,118 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {createPlayerClient, PlayerClient} from "@reveldigital/client-sdk";
+import {reactive} from "vue";
+
+
+  const title = 'ng-test-client-sdk';
+  let sdk: PlayerClient;
+  let revel = reactive({
+    state : 'Not ready',
+    localTime: "",
+    deviceTime: '',
+    TZName: '',
+    TZId: '',
+    TZOffset: '',
+    langCode: '',
+    deviceKey: '',
+    revelRoot: '',
+    remoteDeviceKey: '',
+    commandMap: '',
+    prefs: '',
+    style: ''
+  })
+
+  revel.state = 'ready'// Initialize the SDK
+
+  sdk = createPlayerClient({
+    useLegacyEventHandling: true
+  });
+  //
+  // sdk.on(EventType.START,  ()=> {
+  //   state = 'started'
+  //   console.log('Player started');
+  //   sdk.getDevice().then(function (device) {
+  //     console.log('Device: ' + device);
+  //   })
+  // });
+  //
+  // sdk.on(EventType.COMMAND, function (data) {
+  //   console.log('Command received: ' + data);
+  // });
+
+  sdk.isPreviewMode().then(function (isPreview) {
+    console.log('Is preview mode: ' + isPreview);
+  });
+
+
+  setInterval(() => {
+    update();
+  }, 1000);
+
+  function update() {
+    sdk.getDeviceTime().then((res) => {
+      revel.localTime = (new Date()).toString();
+      // @ts-ignore
+      //todo fix
+      revel.deviceTime = new Date(res);
+    });
+
+    sdk.getDeviceTimeZoneName().then((res) => {
+      revel.TZName = res? res : '';
+    });
+
+    sdk.getDeviceTimeZoneID().then((res) => {
+      revel.TZId = res? res : '';
+    })
+
+    sdk.getDeviceTimeZoneOffset().then((res) => {
+      revel.TZOffset = res? res.toString() : '';
+    });
+
+    sdk.getLanguageCode().then((res) => {
+      revel.langCode = res? res : '';
+    });
+
+    sdk.getDeviceKey().then((res) => {
+      revel.deviceKey = res? res : '';
+    });
+
+    sdk.getRevelRoot().then((res) => {
+      revel.revelRoot = res? res : '';
+    });
+
+    sdk.getCommandMap().then((res) => {
+      revel.commandMap = res? res : '';
+    });
+  }
+
+  function sendCommand() {
+    sdk.sendCommand("test", "it");
+  }
+
+
+
+  function trackEvent() {
+    sdk.track("test", { "a": "b" });
+  }
+
+  function callback() {
+    sdk.callback('test');
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h2>{{title}}</h2>
+  <h2>State: {{ revel.state }}</h2>
+  <h2>Local Time: {{ revel.localTime }}</h2>
+  <h2>Device Time: {{ revel.deviceTime }}</h2>
+  <h2>Device TZ Name: {{ revel.TZName }}</h2>
+  <h2>Device TZ ID: {{ revel.TZId }}</h2>
+  <h2>Device TZ Offset: {{ revel.TZOffset }}</h2>
+  <h2>Device Language Code: {{ revel.langCode }}</h2>
+  <h2>Device Key: {{ revel.deviceKey }}</h2>
+  <h2>Revel Root: {{ revel.revelRoot }}</h2>
+  <h2>Command Map: {{ revel.commandMap }}</h2>
+  <button @click="sendCommand()">Send Command</button>
+  <button @click="trackEvent()">Track Event</button>
+  <button @click="callback()">Callback</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
